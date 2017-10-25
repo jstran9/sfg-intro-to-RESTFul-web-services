19e302de049c0fabbd5d7d1406d82e027c24e87e
ttran9 <toddtran9@gmail.com>
2017-10-25 13:57:22 -0700

    Video 270. Modifying the service to take in reactive.

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -3,9 +3,13 @@ package guru.springframework.springrestclientexamples.services;
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -31,5 +35,19 @@ public class ApiServiceImpl implements ApiService {
 
         UserData userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData.class);
         return userData.getData();
+    }
+
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+
+        // this method doesn't get executed until it's back on the Thymeleaf template being asked for.
+        // so when we run the Thymeleaf template this method gets executed together with the controller's serverwebexchange.
+        return WebClient.create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(resp -> resp.bodyToMono(UserData.class))
+                .flatMapIterable(UserData::getData);
     }
 }
